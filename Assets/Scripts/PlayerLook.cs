15263c3bd3d7acda1b04d71aa192fb24656e0daa
electon100 <cf16670@my.bristol.ac.uk>
2020-04-17 13:09:52 +0100

    First commit, movement working

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    private float mouseSensitivity = 100f;
+
+    private float mouseX = 0f;
+    private float mouseY = 0f;
+
+    private float xRotation = 0f;
+
+    public Transform playerBody;
+
+    void Start()
+    {
+        // Lock the cursor so it doesn't get in the way
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Update mouse input
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Calculate new x rotation and clamp it
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Make changes to the camera and player
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float x = 0f;
+    private float z = 0f;
+
+    private float speed = 6f;
+
+    public CharacterController controller;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
